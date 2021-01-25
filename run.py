b75e5f9b01489e91929c4809621a78f1d181cb97
relarizky <relarizky19@gmail.com>
2021-01-25 13:59:18 +0700

    finished run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+# Author    : Relarizky
+# Github    : https://github.com/relarizky
+# File Name : run.py
+# Last Modified  : 01/25/21, 01:58 PM
+# Copyright © Relarizky 2021
+
+
+from app import app
+
+app.run()
